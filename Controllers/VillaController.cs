728c96332400ee14ce8af9aec9a19878f7d9a80b
Froilán <frolesti4@gmail.com>
2023-08-21 00:13:40 +0200

    MagicVilla CRUD

---
+++ Controllers/VillaController.cs
@@ -0,0 +1,93 @@
+﻿using MagicVilla_API.Datos;
+using MagicVilla_API.Models;
+using MagicVilla_API.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+        //Primer end-point
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<VillaDto>> GetVillas()
+        {
+            return Ok(VillaStore.villaList);
+        }
+
+        //Segon end-point per extreure les dades de la classe VillaStore
+        [HttpGet("id:int", Name ="GetVilla")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<VillaDto> GetVilla(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(v => v.Id == id);
+
+            if(villa == null)
+            {
+                return NotFound();
+            }
+            return Ok(villa);
+        }
+
+        //Primer end-point per afegir informació a la bdd de VillaStore
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<VillaDto> CreateVilla([FromBody] VillaDto villaDto)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if(VillaStore.villaList.FirstOrDefault(v=>v.Name.ToLower() == villaDto.Name.ToLower()) != null)
+            {
+                ModelState.AddModelError("NameExists", "La villa con este nombre ya existe!");
+                return BadRequest(ModelState);
+            }
+            if(villaDto == null)
+            {
+                return BadRequest(villaDto);
+            }
+            if (villaDto.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            villaDto.Id = VillaStore.villaList.OrderByDescending(v => v.Id).FirstOrDefault().Id +1;
+            VillaStore.villaList.Add(villaDto);
+
+            return CreatedAtRoute("GetVilla", new {id=villaDto.Id}, villaDto);
+        }
+
+        //End-point per esborrar informació de la bdd
+        [HttpDelete("{id:int")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult DeleteVilla(int id) 
+        {
+            if (id == 0) {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(v=>v.Id == id);
+            if(villa == null)
+            {
+                return NotFound();
+            }
+            VillaStore.villaList.Remove(villa);
+
+            return NoContent();
+        }
+
+
+    }
+}
---
+++ Datos/VillaStore.cs
@@ -0,0 +1,13 @@
+﻿using MagicVilla_API.Models.Dto;
+
+namespace MagicVilla_API.Datos
+{
+    public static class VillaStore
+    {
+        public static List<VillaDto> villaList = new List<VillaDto>
+        {
+            new VillaDto { Id = 1, Name = "Vista a la piscina" },
+            new VillaDto { Id = 2, Name = "Vista a la playa" }
+        };
+    }
+}
